8181e20edb29d0a58f732f1cc332996ea329ba7d
Novikond <n0vikd@yandex.ru>
2023-07-08 00:11:48 +0300

    Death counter and colored text
    it's bad but i'll fix later

--- mods/SaraHUD/scripts/SaraHUD.lua
+++ mods/SaraHUD/scripts/SaraHUD.lua
@@ -7,15 +7,22 @@ local pref = {
 	statsType = 'sarahud',
 	skin = 'copacetic',
 	
-	fullTimeTxt = true,
+	fullTimeTxt = false,
 	extraStats = false,
 	
+	coloredText = true,
 	modifiersIcons = false,
-	deathCounter = false,
+	deathCounter = true,
 	verticalHealthBar = false,
 	
 	laneUnderlay = 0,
 	elementsAlpha = 1
+}
+
+local textColors = {
+	['ratingText'] = 'f6b7bd',
+	['scoreText'] = 'b1d9f0',
+	['missesText'] = 'e3b7ee'
 }
 -- no more settings :pensive:
 
@@ -23,6 +30,7 @@ local shud = 'saraHUD/'..pref.skin..'/'
 local statsWidth = 20
 
 local hudStuff = {'scoreTxt', 'timeBar', 'timeBarBG', 'timeTxt'}
+local textStuff = {'ratingText', 'scoreText', 'missesText'}
 local statsbgthings = {'leftRounded', 'centerBox', 'rightRounded'}
 local DEFsarahud = {'left', 'hud', 16, 'ffffff', 1, '000000', true, 'PhantomMuff.ttf'}
 
@@ -45,6 +53,12 @@ function onCreatePost()
 		draw.text('scoreText', '0', 0, getProperty('scoreIcon.x')+42, getProperty('scoreIcon.y')+6, unpack(DEFsarahud))
 		draw.text('missesText', '0', 0, getProperty('missesIcon.x')+42, getProperty('missesIcon.y')+6, unpack(DEFsarahud))
 	end
+	
+	if pref.coloredText then
+		setTextColor('ratingText', textColors['ratingText'])
+		setTextColor('scoreText', textColors['scoreText'])
+		setTextColor('missesText', textColors['missesText'])
+	end
 end
 
 function onUpdateScore()
@@ -55,4 +69,20 @@ function onUpdateScore()
 	statsWidth = getProperty('ratingText.width') - 18
 	setGraphicSize('centerBox', statsWidth, 110)
 	setProperty('rightRounded.x', getProperty('centerBox.x')+getProperty('centerBox.width'))
+end
+
+function onGameOverStart()
+	if pref.deathCounter then
+	local deaths = getPropertyFromClass('PlayState', 'deathCounter')
+		draw.sprite('bbIcon', shud..'bbIcon', 15, screenHeight - 45, 'hud', 32)
+		draw.text('bbText', (deaths == 1) and '1 time' or deaths..' times', 0, getProperty('bbIcon.x')+38, getProperty('bbIcon.y')+6, unpack(DEFsarahud))
+	end
+end
+
+function onGameOverConfirm()
+	if pref.deathCounter then
+		effect.shift('bbText', 15, 0, true, 0.5, 'circOut')
+		doTweenAlpha('bbIconbye', 'bbIcon', 0, 1.5, 'circIn')
+		doTweenAlpha('bbTextbye', 'bbText', 0, 1.5, 'circIn')
+	end
 end

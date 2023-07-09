d0f69a75cb8311d141df97e061652a08e53c4c66
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2023-07-09 13:33:33 -0700

    reorganizing and cleaning up

--- scripts/saraTools.lua
+++ scripts/saraTools.lua
@@ -5,7 +5,6 @@
 
 draw = {}
 effect = {}
-preference = {}
 
 draw.text = function(tag, text, width, x, y, alignment, camera, size, color, borderSize, borderColor, antial, font)
 	makeLuaText(tag, text, width, x, y)
@@ -14,9 +13,9 @@ draw.text = function(tag, text, width, x, y, alignment, camera, size, color, bor
 	setTextSize(tag, size or 20)
 	setTextColor(tag, color or 'ffffff')
 	if borderSize or borderColor then setTextBorder(tag, borderSize or 2, borderColor or '000000') end
-	setProperty(tag..'.antialiasing', antial or true)
+	setProperty(tag .. '.antialiasing', antial or true)
 	setTextFont(tag, font or 'vcr.ttf')
-	setObjectOrder(tag, getObjectOrder('healthBar')+100)
+	setObjectOrder(tag, getObjectOrder('healthBar') + 100)
 	addLuaText(tag, true)
 end
 
@@ -25,7 +24,7 @@ draw.sprite = function(tag, image, x, y, camera, sizeX, sizeY, isAnimated)
 	else makeAnimatedLuaSprite(tag, image, x, y) end
 	setObjectCamera(tag, camera or 'hud')
 	if sizeX or sizeY then setGraphicSize(tag, sizeX, sizeY) end
-	setObjectOrder(tag, getObjectOrder('healthBar')+100)
+	setObjectOrder(tag, getObjectOrder('healthBar') + 100)
 	addLuaSprite(tag, true)
 end
 
@@ -37,40 +36,30 @@ draw.graphic = function(tag, x, y, width, height, color, camera)
 end
 
 effect.blink = function(tag, color, speed, ease)
-	setProperty(tag..'.color', getColorFromHex(color))
-	doTweenColor(tag..'-effect-blink', tag, 'ffffff', (speed or 0.2) / playbackRate, ease or 'linear')
+	setProperty(tag .. '.color', getColorFromHex(color))
+	doTweenColor(tag .. '-effect-blink', tag, 'ffffff', (speed or 0.2) / playbackRate, ease or 'linear')
 end
 
 effect.bop = function(tag, scaleX, scaleY, speed, ease)
-	setProperty(tag..'.scale.x', scaleX)
-    setProperty(tag..'.scale.y', scaleY)
-    doTweenX(tag..'-effect-bop-x', tag..'.scale', 1, (speed or 0.2) / playbackRate, ease or 'linear')
-    doTweenY(tag..'-effect-bop-y', tag..'.scale', 1, (speed or 0.2) / playbackRate, ease or 'linear')
+	setProperty(tag .. '.scale.x', scaleX)
+    setProperty(tag .. '.scale.y', scaleY)
+    doTweenX(tag .. '-effect-bop-x', tag .. '.scale', 1, (speed or 0.2) / playbackRate, ease or 'linear')
+    doTweenY(tag .. '-effect-bop-y', tag .. '.scale', 1, (speed or 0.2) / playbackRate, ease or 'linear')
 end
 
 effect.shift = function(tag, moveX, moveY, isAllowed, speed, ease)
 	if isAllowed then og = {x = getProperty(tag..'.x'), y = getProperty(tag..'.y')} end
-	setProperty(tag..'.x', og.x + moveX)
-	setProperty(tag..'.y', og.y + moveY)
-	doTweenX(tag..'-effect-shift-x', tag, og.x, (speed or 0.2) / playbackRate, ease or 'linear')
-	doTweenY(tag..'-effect-shift-y', tag, og.y, (speed or 0.2) / playbackRate, ease or 'linear')
-end
-
-preference.set = function(name, value)
-	name = value
-end
-
-preference.get = function(name)
-	return name
+	setProperty(tag .. '.x', og.x + moveX)
+	setProperty(tag .. '.y', og.y + moveY)
+	doTweenX(tag .. '-effect-shift-x', tag, og.x, (speed or 0.2) / playbackRate, ease or 'linear')
+	doTweenY(tag .. '-effect-shift-y', tag, og.y, (speed or 0.2) / playbackRate, ease or 'linear')
 end
 
 function switch(case_table)
    return function(case_val)
-      local case_fn = case_table[case_val] or case_table.def
-      if case_fn then
-         return case_fn()
-      end
-   end
+      	local case_fn = case_table[case_val] or case_table.def
+      	if case_fn then return case_fn() end
+   	end
 end
 
 function formatTime(millisecond)
@@ -78,4 +67,4 @@ function formatTime(millisecond)
     return string.format("%01d:%02d", (seconds / 60) % 60, seconds % 60)  
 end
 
-return saraTools
+return draw, effect
